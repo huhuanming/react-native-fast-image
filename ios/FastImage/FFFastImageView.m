c40e55a67219a348c3ac964e57935bd5135459c2
huhuanming <workboring@gmail.com>
2017-07-25 01:22:19 +0800

    Update FFFastImageView.m

--- ios/FastImage/FFFastImageView.m
+++ ios/FastImage/FFFastImageView.m
@@ -7,50 +7,64 @@
     if (_resizeMode != resizeMode) {
         _resizeMode = resizeMode;
         self.contentMode = (UIViewContentMode)resizeMode;
+        [self reloadImage:_source];
     }
+}
+    
+- (void)setOnFastImageError:(RCTDirectEventBlock)onFastImageError {
+    _onFastImageError = onFastImageError;
+    [self reloadImage:_source];
+}
+    
+- (void)setOnFastImageLoad:(RCTDirectEventBlock)onFastImageLoad {
+    _onFastImageLoad = onFastImageLoad;
+    [self reloadImage:_source];
+}
+    
+- (void)reloadImage:(FFFastImageSource *)source {
+    [source.headers enumerateKeysAndObjectsUsingBlock:^(NSString *key, NSString* header, BOOL *stop) {
+        [[SDWebImageDownloader sharedDownloader] setValue:header forHTTPHeaderField:key];
+    }];
+    
+    // Set priority.
+    SDWebImageOptions options = 0;
+    options |= SDWebImageRetryFailed;
+    switch (source.priority) {
+        case FFFPriorityLow:
+        options |= SDWebImageLowPriority;
+        break;
+        case FFFPriorityNormal:
+        // Priority is normal by default.
+        break;
+        case FFFPriorityHigh:
+        options |= SDWebImageHighPriority;
+        break;
+    }
+    
+    // Load the new source.
+    [self sd_setImageWithURL:source.uri
+            placeholderImage:nil
+                     options:options
+                   completed:^(UIImage *image,
+                               NSError *error,
+                               SDImageCacheType cacheType,
+                               NSURL *imageURL) {
+                       if (error) {
+                           if (_onFastImageError) {
+                               _onFastImageError(@{});
+                           }
+                       } else {
+                           if (_onFastImageLoad) {
+                               _onFastImageLoad(@{});
+                           }
+                       }
+                   }];
 }
 
 - (void)setSource:(FFFastImageSource *)source {
     if (_source != source) {
         _source = source;
-        // Set headers.
-        [source.headers enumerateKeysAndObjectsUsingBlock:^(NSString *key, NSString* header, BOOL *stop) {
-            [[SDWebImageDownloader sharedDownloader] setValue:header forHTTPHeaderField:key];
-        }];
-
-        // Set priority.
-        SDWebImageOptions options = 0;
-        options |= SDWebImageRetryFailed;
-        switch (source.priority) {
-            case FFFPriorityLow:
-                options |= SDWebImageLowPriority;
-                break;
-            case FFFPriorityNormal:
-                // Priority is normal by default.
-                break;
-            case FFFPriorityHigh:
-                options |= SDWebImageHighPriority;
-                break;
-        }
-
-        // Load the new source.
-        [self sd_setImageWithURL:source.uri
-                placeholderImage:nil
-                         options:options
-                       completed:^(UIImage *image,
-                                   NSError *error,
-                                   SDImageCacheType cacheType,
-                                   NSURL *imageURL) {
-                           if (error) {
-                             if (_onFastImageError) {
-                               _onFastImageError(@{});
-                             }
-                           } else {
-                             if (_onFastImageLoad) {
-                               _onFastImageLoad(@{});
-                             }
-                           }
-                       }];
+        [self reloadImage:source];
     }
 }
 
